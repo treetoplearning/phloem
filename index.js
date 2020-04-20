9f5a3ada2a27981d39ae46c394c5c2c868fcdf37
Jacob Patel <jseanpatel@gmail.com>
2020-04-20 16:25:11 -0700

    working twilio query

---
+++ index.js
@@ -0,0 +1,12 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+  });
+
+  console.log("connected: you never listen");
+  ws.send("something");
+});
